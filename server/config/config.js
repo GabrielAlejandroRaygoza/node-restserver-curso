15d295a6c4335052a0becd01aefda44655527c15
Gabriel_Raygoza <graygoza@fragua.com.mx>
2018-11-05 13:03:09 -0600

    RestServer - usuario

--- server/config/config.js
+++ server/config/config.js
@@ -1,4 +1,22 @@
 //================================
 //PUERTO
-process.env.PORT = process.env.PORT || 3000;
 //================================
+process.env.PORT = process.env.PORT || 3000;
+
+//================================
+//ENTORNO
+//================================
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+//================================
+//BASE DE DATOS
+//================================
+
+let urlDB;
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost:27017/cafe'
+} else {
+    urlDB = 'mongodb://usercafe:usercafe1@ds151943.mlab.com:51943/cafe';
+}
+
+process.env.URLDB = urlDB;
