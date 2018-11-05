e44debcc001fee9746dfc1462d395cac65b54cb8
Gabriel_Raygoza <graygoza@fragua.com.mx>
2018-11-05 13:34:04 -0600

    Variabale de entorno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -16,7 +16,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe'
 } else {
-    urlDB = 'mongodb://usercafe:usercafe1@ds151943.mlab.com:51943/cafe';
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
